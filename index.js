aa15324670356cb0ae9c0f391803d82b670fee95
ToastedGMS <gabrielsiqueira3052@gmail.com>
2023-03-16 11:54:13 -0300

    added computer choice function

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+function getComputerChoice() {
+const arr = ['Rock', 'Paper', 'Scissors']
+const choice = arr[(Math.floor(Math.random() * (arr.length)))]
+return choice
+}
+
+console.log(getComputerChoice());
